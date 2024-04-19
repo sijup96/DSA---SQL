bf4b6e90ae14353fdd6537bf853b823d3348cd51
sijup96 <sijup96@gmail.com>
2024-04-19 20:05:38 +0530

    DFS & BFS

--- DSA/tree/binarySearchTree.js
+++ DSA/tree/binarySearchTree.js
@@ -49,13 +49,52 @@ class binarySearchTree {
       }
     }
   }
+  preOrder(root){
+if(root){
+  console.log('preOrder: ',root.value);
+  this.preOrder(root.left)
+  this.preOrder(root.right)
+}
+  }
+  inOrder(root){
+    if(root){
+      this.inOrder(root.left)
+      console.log('inorder',root.value);
+      this.inOrder(root.right)
+    }
+  }
+  postOrder(root){
+if(root){
+  this.postOrder(root.left)
+  this.postOrder(root.right)
+  console.log('postOrder',root.value);
+}
+  }
+  levelOrder(){
+    const queue=[]
+    queue.push(this.root)
+    while(queue.length){
+      let curr=queue.shift()
+      console.log('levelOrder:',curr.value);
+      if(curr.left){
+        queue.push(curr.left)
+      }
+      if(curr.right)
+      queue.push(curr.right)
+    }
+  }
 }
 
-
+const arr=[10,5,15,3,7]
 const bst = new binarySearchTree();
 console.log("isEmpty: ", bst.isEmpty());
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
+for(let i=0;i<arr.length;i++){
+  bst.insert(arr[i]);
+}
+
 console.log("isEmpty: ", bst.isEmpty());
 console.log(bst.search(bst.root, 15));
+bst.preOrder(bst.root)
+bst.inOrder(bst.root)
+bst.postOrder(bst.root)
+bst.levelOrder()
