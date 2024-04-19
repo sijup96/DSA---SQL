8ca8538bc9c8289fbf1f4bdb91fb7e971fc695bd
sijup96 <sijup96@gmail.com>
2024-04-19 15:01:18 +0530

    binary search tree

---
+++ DSA/tree/binarySearchTree.js
@@ -0,0 +1,61 @@
+// BST O(log n)
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class binarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  isEmpty() {
+    return this.root === null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+  search(root, value) {
+    if (!root) return false;
+    if (root.value === value) {
+      return true;
+    } else {
+      if (value < root.value) {
+        return this.search(root.left, value);
+      } else {
+        return this.search(root.right, value);
+      }
+    }
+  }
+}
+
+
+const bst = new binarySearchTree();
+console.log("isEmpty: ", bst.isEmpty());
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+console.log("isEmpty: ", bst.isEmpty());
+console.log(bst.search(bst.root, 15));
